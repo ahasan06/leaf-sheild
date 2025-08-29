369b2a97299ea6ed489b5112e46301caba7c11ae
Md Ahasan Habib Nahid <141475377+ahasan06@users.noreply.github.com>
2025-08-30 02:22:58 +0600

    update

--- app.py
+++ app.py
@@ -223,4 +223,7 @@ def favicon():
     return ("", 204)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  
+    debug = os.environ.get("FLASK_DEBUG") == "1"
+    app.run(host="0.0.0.0", port=port, debug=debug)
